81aa001b5a6dc86523cbf232f3a4a4c646a7e3cd
Ajeet <chaubeyajeet8@gmail.com>
2024-05-25 11:16:30 +0530

    First commit

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\Admin\AppController as AdminAppController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[AppController::class,'index'])->name("index");
+
+Route::get('/about-us', [AppController::class,'aboutUs'])->name("about-us");
+
+Route::get('/contact-us', [AppController::class,'contactUs'])->name("contact-us");
+
+Route::get('/blogs', [AppController::class,'blogs'])->name("blogs");
+
+Route::get('/blog/{slug}', [AppController::class, 'blog']) ->name("blog");
+
+
+
+##admin routes
+
+Route::group(['prefix'=>'admin'],function(){
+
+    Route::get('dashboard',[AdminAppController::class,'dashboard'])->name("dashboard");
+
+    Route::get('categories',[AdminAppController::class,'categories'])->name("categories");
+    Route::get('add-categories',[AdminAppController::class,'addCategory'])->name("categories.add");
+
 });
